9bf5d75c449da9222f30f619ec46a92099e0fe25
Rafael <rvitall.alves@gmail.com>
2020-05-17 23:23:59 -0300

    object placement

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private float _gridSize = 1;
+    private Vector3[] _grid;
+    public Vector3 GetPointOnGrid (Vector3 v) 
+    {
+        v.x = v.x - v.x % _gridSize;
+        v.y = v.y - v.y % _gridSize;
+        v.z = v.z - v.z % _gridSize;
+        return v;
+    }
+}
---
+++ Assets/Scripts/ObjectPlacer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlacer : MonoBehaviour
+{
+    [SerializeField] private GameObject _objectPrefab;
+    [SerializeField] private Transform _objectPlacePos;
+    [SerializeField] private GridManager gridManager;
+
+    private void Update() {
+        if (Input.GetKeyDown (KeyCode.Space))
+        {
+            PlaceObject();
+        }
+    }
+
+    private void PlaceObject()
+    {
+        Vector3 objectPos = gridManager.GetPointOnGrid(_objectPlacePos.position);
+        Instantiate(_objectPrefab, objectPos, Quaternion.identity);
+    }
+}
